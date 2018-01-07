ed720ff93762c5064af883b17f9c7927c7ef3f84
Surya Kandikonda <34576740+suryakandikonda@users.noreply.github.com>
2018-01-07 13:28:27 +0530

    Add files via upload

---
+++ rock-paper-scissor-lizard-spock mini project.py
@@ -0,0 +1,56 @@
+# Rock-paper-scissors-lizard-Spock template
+#Coded by : SURYA KANDIKONDA
+
+import random
+
+def name_to_number(name):
+    if name == "rock":
+        return 0
+    elif name == "Spock":
+        return 1
+    elif name == "paper":
+        return 2
+    elif name == "lizard":
+        return 3
+    elif name == "scissors":
+        return 4
+    else:
+        return "Something wrong with your Imput"
+
+
+def number_to_name(number):
+    if number == 0:
+        return "rock"
+    elif number == 1:
+        return "Spock"
+    elif number == 2:
+        return "paper"
+    elif number == 3:
+        return "lizard"
+    elif number == 4:
+        return "scissors"
+    else:
+        return "Something wrong with the input"
+
+def rpsls(player_choice):
+    print " "
+    print " " 
+    print "Player chooses " + player_choice
+    player_number=name_to_number(player_choice)
+    comp_number = random.randrange(0,4)
+    comp_choice=number_to_name(comp_number)
+    print "Computer chooses " + comp_choice
+    rel = (comp_number - player_number)%5
+    if(rel == 1 or rel == 2):
+        print "Computer Wins"
+    elif(rel == 3 or rel == 4):
+        print "Player Wins"
+    elif(rel == 0):
+        print "Player and Computer Tie"
+    
+#TEST CASES.  IGNORE THEM    
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
